a01817aede2310cd46e8cf69dd2040b8cead9d67
Italo Santos <italosantoscont@gmail.com>
2026-01-20 23:09:29 -0300

    Iniciando projeto

---
+++ src/main/java/com/valorize/valorize/ValorizeApplication.java
@@ -0,0 +1,13 @@
+package com.valorize.valorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valorize/valorize/ValorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorize.valorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
